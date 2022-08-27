7e9c8a2beec7dad6c714a45effd57551a5ecd8aa
Matt Breckon <mattbreckon@gmail.com>
2022-08-27 13:33:22 -0400

    Remove unused util const

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,6 @@
 // map imported require.context files to key-value map of file name and webpack file path
-export function importAllFiles(r: __WebpackModuleApi.RequireContext) {
-  return r.keys().reduce((acc, val) => ({ ...acc, [val.replace('./', '')]: r(val).default }), {});
-}
-
-export const requireContextMediaDefaults: [boolean, RegExp] = [false, /\.(gif|png|jpe?g|svg|mp4)$/i];
+export const importAllFiles = (r: __WebpackModuleApi.RequireContext) =>
+  r.keys().reduce((acc, val) => ({ ...acc, [val.replace('./', '')]: r(val).default }), {});
 
 // validate if filename is a supported image type
 export const isImage = (file: string) => /\.(gif|png|jpe?g|svg)$/i.test(file);
